98dc9e8844ffa4a013f6e63760e51c30134adba5
RawatShreyansh <100209808+RawatShreyansh@users.noreply.github.com>
2024-02-12 06:39:05 +0530

    Time: 3 ms (99.85%) | Memory: 7.6 MB (99.94%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,11 +1,41 @@
+int init = [] {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ofstream out("user.out");
+
+    for (string s; getline(cin, s); out << '\n') {
+        int ans = 0;
+        int c = 0;
+
+        for (int _i = 1, _n = s.length(); _i < _n; ++_i) {
+            bool _neg = false;
+
+            if (s[_i] == '-') {
+                ++_i;
+                _neg = true;
+            }
+
+            int v = s[_i++] & 15;
+
+            while ((s[_i] & 15) < 10)
+                v = v * 10 + (s[_i++] & 15);
+
+            if (_neg)
+                v = -v;
+
+            if (c == 0)
+                ans = v;
+            
+            ans == v ? ++c : --c;
+        }
+        out << ans;
+    }
+    out.flush();
+    exit(0);
+    return 0;
+}();
 class Solution {
 public:
-    const int inti = []() {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
-        return 0;
-    }();
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
 
