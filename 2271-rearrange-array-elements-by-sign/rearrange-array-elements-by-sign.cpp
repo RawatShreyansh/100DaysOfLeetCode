c9379c3124d733a90a4e1d85a3ee8321f84e9c9c
RawatShreyansh <100209808+RawatShreyansh@users.noreply.github.com>
2024-02-15 02:34:42 +0530

    Time: 89 ms (99.68%) | Memory: 125.9 MB (67.24%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>v1,v2,ans;
-        
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]>0)v1.push_back(nums[i]);
-            else v2.push_back(nums[i]);
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
+        int n = nums.size();
+        vector<int> ans(n);
+        int i=0,j=1;
+        for(int k=0;k<n;k++){
+            if(nums[k]>0){
+                ans[i] = nums[k];
+                i+=2;
+            }
+            else{
+                ans[j] = nums[k];
+                j+=2; 
+            }
         }
-        int ind1=0,ind2=0;
-        
 
-        while(ind2<nums.size()/2){
-            ans.push_back(v1[ind1]);
-            ind1++;
-            ans.push_back(v2[ind2]);
-            ind2++;
-        }
         return ans;
     }
 };
-
