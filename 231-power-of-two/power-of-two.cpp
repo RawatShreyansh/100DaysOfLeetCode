4cb6c871143cf5b6e3db32ba24f8957c8a3f925a
RawatShreyansh <100209808+RawatShreyansh@users.noreply.github.com>
2024-02-19 16:08:16 +0530

    Time: 4 ms (22.67%) | Memory: 7.2 MB (19.05%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -11,18 +11,7 @@ public:
 
         if(n <= 0)
             return false;
-        
-        vector<long long> power;
-        power.push_back(1);
-        for (long long i = 1; i < 33; i++) {
-            power.push_back(power[i - 1] * 2);
-        }
 
-        for (auto& i : power) {
-            if (i == n)
-                return true;
-        }
-
-        return false;
+        return (n&(n-1)) == 0;
     }
 };
