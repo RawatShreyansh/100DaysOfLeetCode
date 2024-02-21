d593d0b95c585eedfb513ef35ce84a9f853ad07c
RawatShreyansh <100209808+RawatShreyansh@users.noreply.github.com>
2024-02-21 21:01:21 +0530

    Time: 7 ms (64.93%) | Memory: 9 MB (8.30%) - LeetSync

---
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,18 @@
+const int init = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+        return (left << cnt);
+    }
+};
