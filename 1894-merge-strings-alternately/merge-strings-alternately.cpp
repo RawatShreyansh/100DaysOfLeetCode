55c58952dbd46e9a25865457ef3dd76284bfe7a1
RawatShreyansh <100209808+RawatShreyansh@users.noreply.github.com>
2024-02-24 22:45:16 +0530

    Time: 3 ms (48.65%) | Memory: 7.7 MB (14.45%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,27 @@
+const int init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+
+        int n = word1.size();
+        int m = word2.size();
+
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            if (i < n)
+                res += word1[i];
+            if (j < m)
+                res += word2[j];
+            i++;
+            j++;
+        }
+        return res;
+    }
+};
