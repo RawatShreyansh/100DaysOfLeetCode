a45c02c071ee0e408d5551f4b9edbe80c6eb474a
RawatShreyansh <100209808+RawatShreyansh@users.noreply.github.com>
2024-03-14 01:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (15.68%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -9,16 +9,15 @@ const int init = [](){
 class Solution {
 public:
     int pivotInteger(int n) {
+        int total = (n*(n+1))/ 2;
         for(int i = 1 ; i <= n ; i++){
-            int left = 0 , right = 0;
-            for(int j = 1; j <= i ; ++j){
-                left += j;
-            }
-            for(int k = i ; k <= n ; k++){
-                right += k;
-            }
+            
+            int left = (i*(i+1)) /2;
+            int right = total - left + i;
+
             if(left == right)
                 return i;
+           
         }
         return -1;
     }
