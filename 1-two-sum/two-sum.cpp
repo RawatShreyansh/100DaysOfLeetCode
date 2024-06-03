61a712c6fd2f01c9eca3d1cbf40152fb11ff0421
RawatShreyansh <100209808+RawatShreyansh@users.noreply.github.com>
2024-06-04 02:04:09 +0530

    Time: 8 ms (78.63%) | Memory: 14 MB (29.95%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,7 +5,6 @@ public:
         for(int i = 0 ; i <nums.size() ; ++i){
             int newTarget = target-nums[i];
             auto it = mp.find(newTarget);
-
             if(it != mp.end()){
                 return {i,it->second};
             }
