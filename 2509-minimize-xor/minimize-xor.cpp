1279ccb9f2ac2e9e0bd3c85014e695af6a7d6698
Shreyansh Rawat <100209808+RawatShreyansh@users.noreply.github.com>
2025-01-16 00:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (10.75%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,25 @@
+const int init = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int a = __builtin_popcount(num1);
+        int b = __builtin_popcount(num2);
+        int res = num1;
+        for (int i = 0; i < 32; ++i) {
+            if (a > b && (1 << i) & num1) {
+                res ^= 1 << i;
+                --a;
+            }
+            if (a < b && !((1 << i) & num1)) {
+                res ^= 1 << i;
+                ++a;
+            }
+        }
+        return res;
+    }
+};
