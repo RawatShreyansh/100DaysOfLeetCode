36f7d3396ca8a7904c13fdd3d15f3e6cd81691d0
Shreyansh Rawat <100209808+RawatShreyansh@users.noreply.github.com>
2025-02-22 18:48:46 +0530

    Time: 51 ms (21.74%) | Memory: 75.6 MB (20.80%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,26 +1,21 @@
-
-const int init = []() {
+const char init = [](){
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 'c';
 }();
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = static_cast <int> (nums.size()) ;
-        if ( n == 1 ) return false ;
-        std::sort (nums.begin(), nums.end()) ;
-        int i {0} ;
-        int j {i+1};
-        while ( i < n - 1) {
-            for ( j = i + 1 ; j < n ; ++ j) {
-                if ( nums [j] != nums [i] ) break ;
-            }
-            if ( j - i > 1 ) return true ;
-            i = j;
+        unordered_map<int,int> mp;
+        for(int &i : nums){
+            mp[i]++;
         }
-        return false ;
-        
+        for(auto &i : mp){
+            if(i.second > 1){
+                return true;
+            }
+        }
+        return false;
     }
 };
